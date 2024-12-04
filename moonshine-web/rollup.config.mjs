62c5638b6aa01bbabe48e7d63b722982e8dc8271
Evan King <e.king@utexas.edu>
2024-12-04 13:01:14 -0600

    Refactor to separate into components

--- moonshine-web/rollup.config.mjs
+++ moonshine-web/rollup.config.mjs
@@ -1,13 +1,16 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
-export default {
-  input: "src/index.js",
-  external: ["onnxruntime-web"], // assumes onnxruntime-web <script> will be included separately
-  output: {
-    file: "dist/moonshine.min.js",
-    format: "iife",
-    name: "UsefulMoonshine",
+export default [
+  // manual moonshine-ify (using data-moonshine-target)
+  {
+    input: "src/manual.js",
+    external: ["onnxruntime-web"], // assumes onnxruntime-web <script> will be included separately
+    output: {
+      file: "dist/moonshine.min.js",
+      format: "iife",
+      name: "UsefulMoonshine",
+    },
+    plugins: [nodeResolve({browser: true}), terser()]
   },
-  plugins: [nodeResolve({browser: true}), terser()]
-};
+];
