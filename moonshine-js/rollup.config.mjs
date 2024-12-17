e2c571c1cc2e1d2e47fe72978a02ee9a6f38cb36
Evan King <e.king@utexas.edu>
2024-12-17 09:23:52 -0700

    Import CSS and SVG assets instead of inlining

--- moonshine-js/rollup.config.mjs
+++ moonshine-js/rollup.config.mjs
@@ -1,35 +1,47 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import css from "rollup-plugin-import-css";
+import svg from "rollup-plugin-svg-import";
+
+const plugins = [
+    nodeResolve({ browser: true }),
+    typescript(),
+    svg({
+      stringify: true
+    }),
+    css(),
+    terser(),
+];
 
 export default [
-  // moonshine module for development/npm
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/moonshine.min.js",
-      format: "es"
+    // moonshine module for development/npm
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/moonshine.min.js",
+            format: "es",
+        },
+        plugins: plugins,
     },
-    plugins: [nodeResolve({browser: true}), typescript(), terser()]
-  },
-  // manual moonshine-ify from CDN
-  {
-    input: "src/manual.ts",
-    output: {
-      file: "dist/moonshine.manual.min.js",
-      format: "iife",
-      name: "UsefulMoonshine",
+    // manual moonshine-ify from CDN
+    {
+        input: "src/manual.ts",
+        output: {
+            file: "dist/moonshine.manual.min.js",
+            format: "iife",
+            name: "UsefulMoonshine",
+        },
+        plugins: plugins,
     },
-    plugins: [nodeResolve({browser: true}), typescript(), terser()]
-  },
-  // auto moonshine-ify from CDN
-  {
-    input: "src/auto.ts",
-    output: {
-      file: "dist/moonshine.auto.min.js",
-      format: "iife",
-      name: "UsefulMoonshine",
+    // auto moonshine-ify from CDN
+    {
+        input: "src/auto.ts",
+        output: {
+            file: "dist/moonshine.auto.min.js",
+            format: "iife",
+            name: "UsefulMoonshine",
+        },
+        plugins: plugins,
     },
-    plugins: [nodeResolve({browser: true}), typescript(), terser()]
-  }
 ];
--- moonshine-js/src/constants.ts
+++ moonshine-js/src/constants.ts
@@ -1,9 +1,7 @@
 export const MoonshineSettings = {
     FRAME_SIZE: 250,
     LOOKBACK_FRAMES: 5,
-    // MAX_SPEECH_SECS: 5,
-    // MIN_REFRESH_SECS: 0.3,
-    LOOKBACK_SIZE: 500 * 5,
+    MAX_SPEECH_SECS: 5,
     MAX_RECORD_MS: 30000,
     BASE_ASSET_PATH: "/dist/"
 }
