4a81a919aea04b86e4f16bea1ed0537ed6e21ce2
Evan King <e.king@utexas.edu>
2025-01-13 13:31:02 -0600

    Add settings (model size and enable everywhere)

--- demo/moonshine-js/browser-extension/src/index.ts
+++ demo/moonshine-js/browser-extension/src/index.ts
@@ -12,42 +12,46 @@ if (typeof chrome !== "undefined") {
 
 import { MoonshineElementManager } from "@usefulsensors/moonshine";
 
-var inputAreaSelectors: Array<string> | undefined = undefined;
-var styleSheet: string | undefined = undefined;
-var postInjectionFunction: Function | undefined = undefined;
+chrome.storage.sync.get({ model: "tiny", enableEverywhere: false }, (settings) => {
+    if (settings.enableEverywhere || window.location.host.includes("chatgpt.com") || window.location.host.includes("claude.ai") ) {
+        var inputAreaSelectors: Array<string> | undefined = undefined;
+        var styleSheet: string | undefined = undefined;
+        var postInjectionFunction: Function | undefined = undefined;
 
-switch (window.location.host) {
-    case "chatgpt.com":
-        styleSheet = `
-        .moonshine-container {
-            position: inherit !important;
-            display: inherit !important;
+        switch (window.location.host) {
+            case "chatgpt.com":
+                styleSheet = `
+                .moonshine-container {
+                    position: inherit !important;
+                    display: inherit !important;
+                }
+                .moonshine-button {
+                    top: 8px !important;
+                    right: 11px !important;
+                }
+                @media (prefers-color-scheme: dark) {
+                    .moonshine-button span svg path {
+                        fill: white;
+                    }
+                }
+                @media (prefers-color-scheme: light) {
+                    .moonshine-button span svg path {
+                        fill: black;
+                    }
+                }
+                `;
+                inputAreaSelectors = ["#prompt-textarea"];
+                postInjectionFunction = (controlElement, targetInputElement) => {};
         }
-        .moonshine-button {
-            top: 8px !important;
-            right: 11px !important;
-        }
-        @media (prefers-color-scheme: dark) {
-            .moonshine-button span svg path {
-                fill: white;
-            }
-        }
-        @media (prefers-color-scheme: light) {
-            .moonshine-button span svg path {
-                fill: black;
-            }
-        }
-        `;
-        inputAreaSelectors = ["#prompt-textarea"];
-        postInjectionFunction = (controlElement, targetInputElement) => {};
-}
-
-var elementManager = new MoonshineElementManager(
-    "model/tiny",
-    styleSheet
-);
-setInterval(() => {
-    // re-autoinject every second, since some elements may not exist on page load (e.g., in react-based sites)
-    elementManager.autoInjectElements(inputAreaSelectors, postInjectionFunction);
-    elementManager.initControlElements();
-}, 1000);
+        
+        var elementManager = new MoonshineElementManager(
+            "model/" + settings.model,
+            styleSheet
+        );
+        setInterval(() => {
+            // re-autoinject every second, since some elements may not exist on page load (e.g., in react-based sites)
+            elementManager.autoInjectElements(inputAreaSelectors, postInjectionFunction);
+            elementManager.initControlElements();
+        }, 1000);
+    }
+})
