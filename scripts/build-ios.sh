ee33b167f3a223e364475649ae7d63fca5b5f702
Pete Warden <pete@usefulsensors.com>
2025-12-31 15:56:49 -0800

    Build both iOS targets

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -ex
 
-if [ -z $1 ]; then
-	TARGET=phone
-elif [ $1 == "simulator" ]; then
-	TARGET=simulator
-elif [ $1 == "phone" ]; then
-	TARGET=phone
-else
-	echo "Usage: $0 <phone|simulator>"
-	exit 1
-fi
-
 IOS_VERSION=15.1
 
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
@@ -21,23 +10,22 @@ CORE_BUILD_DIR=${CORE_DIR}/build
 cd ${CORE_DIR}
 find . -type d -name build -exec rm -rf {} +
 
-if [ $TARGET == "phone" ]; then
-	cmake -B build \
-		-G Xcode \
-		-DCMAKE_SYSTEM_NAME=iOS \
-		-DCMAKE_OSX_DEPLOYMENT_TARGET=${IOS_VERSION} \
-		-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
-		.
-else
-	cmake -B build \
-		-G Xcode \
-		-DCMAKE_SYSTEM_NAME=iOS \
-		-DCMAKE_OSX_SYSROOT=iphonesimulator \
-		-DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
-		-DCMAKE_OSX_DEPLOYMENT_TARGET=${IOS_VERSION} \
-		-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
-		.
-fi
-
+mkdir -p ${CORE_BUILD_DIR}
 cd ${CORE_BUILD_DIR}
-cmake --build . --config Release
+cmake -B build-phone \
+	-G Xcode \
+	-DCMAKE_SYSTEM_NAME=iOS \
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=${IOS_VERSION} \
+	-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
+	..
+cmake --build build-phone --config Release
+
+cmake -B build-simulator \
+	-G Xcode \
+	-DCMAKE_SYSTEM_NAME=iOS \
+	-DCMAKE_OSX_SYSROOT=iphonesimulator \
+	-DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=${IOS_VERSION} \
+	-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
+	..
+cmake --build build-simulator --config Release
