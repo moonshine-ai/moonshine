0df337b51f419c30bf4cc99f8a9e395a33975ac2
Pete Warden <pete@usefulsensors.com>
2026-01-01 18:10:00 -0800

    Created MicTranscriber implementation and example for Swift

--- examples/macos/MicTranscription/Package.swift
+++ examples/macos/MicTranscription/Package.swift
@@ -2,20 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "BasicTranscription",
+    name: "MicTranscription",
     platforms: [.macOS(.v13)],
     dependencies: [
         // Uncomment this back in when you want to use the locally-built Swift package.
-        .package(path: "../../../swift")
-        // .package(url: "https://github.com/moonshine-ai/moonshine-swift.git", from: "0.0.16")
+        // .package(path: "../../../swift")
+        .package(url: "https://github.com/moonshine-ai/moonshine-swift.git", from: "0.0.16")
     ],
     targets: [
         .executableTarget(
-            name: "BasicTranscription",
+            name: "MicTranscription",
             dependencies: [
                 // Uncomment this back in when you want to use the locally-built Swift package.
-                .product(name: "Moonshine", package: "swift")
-                // .product(name: "Moonshine", package: "moonshine-swift")
+                // .product(name: "Moonshine", package: "swift")
+                .product(name: "Moonshine", package: "moonshine-swift")
             ]
         )
     ]
