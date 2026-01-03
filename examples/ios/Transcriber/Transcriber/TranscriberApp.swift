5021fd54435adb3f027d58bb8a8f98508f42bda0
Pete Warden <pete@usefulsensors.com>
2026-01-03 13:30:33 -0800

    Implemented Swift static library

---
+++ examples/ios/Transcriber/Transcriber/TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriberApp.swift
+//  Transcriber
+//
+//  Created by Pete Warden on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
