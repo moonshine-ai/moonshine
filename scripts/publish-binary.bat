f4c9d5e4c4a94050950b9ee027b74fbfaaf177ef
Pete Warden <pete@usefulsensors.com>
2026-01-30 11:10:01 -0800

    Bumped version

--- scripts/publish-binary.bat
+++ scripts/publish-binary.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set VERSION=0.0.39
+set VERSION=0.0.40
 set REPO=moonshine-ai/moonshine-v2
 
 REM Get the directory where this script is located
--- scripts/publish-swift.sh
+++ scripts/publish-swift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 FRAMEWORK_NAME="Moonshine"
-VERSION="0.0.39"
+VERSION="0.0.40"
 REPO="moonshine-ai/moonshine-swift"
 
 # Check that the XCFramework exists
