c94c3bdb7f35e48b2a21bf662ed732d623a7419e
Pete Warden <pete@usefulsensors.com>
2026-02-02 14:26:00 -0800

    Added runnable example to intent recognizer class

--- core/transcriber.h
+++ core/transcriber.h
@@ -103,7 +103,7 @@ struct TranscriberOptions {
   float vad_threshold = 0.5f;  
   float vad_window_duration = 0.5f;
   int32_t vad_hop_size = 512;
-  size_t vad_look_behind_sample_count = 4096;
+  size_t vad_look_behind_sample_count = 8192;
   float vad_max_segment_duration = 15.0f;
   float max_tokens_per_second = 6.5f;
   std::string save_input_wav_path = "";
