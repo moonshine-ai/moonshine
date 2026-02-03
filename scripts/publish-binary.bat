fae1f5fc4c9b298a6b7d35d1a224c0db53f67039
Pete Warden <pete@usefulsensors.com>
2026-02-02 19:22:21 -0800

    Bumped version

--- scripts/publish-binary.bat
+++ scripts/publish-binary.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set VERSION=0.0.43
+set VERSION=0.0.44
 set REPO=moonshine-ai/moonshine-v2
 
 REM Get the directory where this script is located
--- scripts/publish-binary.sh
+++ scripts/publish-binary.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -ex
 
-VERSION=0.0.43
+VERSION=0.0.44
 REPO="moonshine-ai/moonshine-v2"
 
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
