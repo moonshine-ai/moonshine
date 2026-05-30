6a754569a2cd5105f8e8caa7c6475487a050b348
Pete Warden <pete@usefulsensors.com>
2026-05-29 18:46:18 -0700

    Fixes for Windows example

--- examples/windows/cli-transcriber/download-lib.bat
+++ examples/windows/cli-transcriber/download-lib.bat
@@ -1,4 +1,6 @@
 curl -L https://github.com/moonshine-ai/moonshine/releases/latest/download/moonshine-voice-windows-x86_64.tar.gz -o moonshine-voice-windows-x86_64.tar.gz
-del /q moonshine-voice-windows-x86_64
+if errorlevel 1 exit /b 1
+if exist moonshine-voice-windows-x86_64 rmdir /s /q moonshine-voice-windows-x86_64
 tar -xzf moonshine-voice-windows-x86_64.tar.gz
+if errorlevel 1 exit /b 1
 del /q moonshine-voice-windows-x86_64.tar.gz
