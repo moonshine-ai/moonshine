11e42241310c1cbcb9a5e6394628cb215f798643
Pete Warden <pete@usefulsensors.com>
2026-07-10 13:14:46 -0700

    Fixed formatting

--- core/moonshine-utils/file-utils.cpp
+++ core/moonshine-utils/file-utils.cpp
@@ -15,9 +15,8 @@ std::size_t fread_exact(void *ptr, std::size_t size, std::size_t count,
   const std::size_t read_count = std::fread(ptr, size, count, stream);
   if (read_count != count) {
     std::ostringstream oss;
-    oss << "Failed to read " << (what ? what : "file") << ": expected "
-        << count << " element(s) of size " << size << " but read only "
-        << read_count;
+    oss << "Failed to read " << (what ? what : "file") << ": expected " << count
+        << " element(s) of size " << size << " but read only " << read_count;
     if (stream != nullptr && std::feof(stream)) {
       oss << " (unexpected end of file)";
     } else if (stream != nullptr && std::ferror(stream)) {
