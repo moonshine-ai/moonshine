fa6b506500e5e505498bcecce394b20fe2fbecba
Pete Warden <pete@usefulsensors.com>
2026-07-23 17:46:50 -0700

    Update to version 0.0.73

--- scripts/publish-binary.sh
+++ scripts/publish-binary.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -ex
 
-VERSION=0.0.72
+VERSION=0.0.73
 REPO="moonshine-ai/moonshine"
 
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
--- scripts/publish-swift.sh
+++ scripts/publish-swift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 FRAMEWORK_NAME="Moonshine"
-VERSION="0.0.72"
+VERSION="0.0.73"
 REPO="moonshine-ai/moonshine-swift"
 
 # Check that the XCFramework exists
